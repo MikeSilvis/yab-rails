1bfd9efae159092d7b6ae6cfefde928ed596b318
Mike Silvis <mikesilvis@gmail.com>
2014-03-30 20:49:15 -0700

    updating ga

--- app/assets/javascripts/lib/google_analytics.js
+++ app/assets/javascripts/lib/google_analytics.js
@@ -9,3 +9,12 @@
  ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+_gaq.push(['_setAccount', 'UA-48488503-2']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+ga.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'stats.g.doubleclick.net/dc.js';
+var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
