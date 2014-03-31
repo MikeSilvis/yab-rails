e77c2d797f95bf13a52cb16cf39f6024208772f7
Mike Silvis <mikesilvis@gmail.com>
2014-03-30 21:38:54 -0700

    adding bug tracking

--- app/admin/location.rb
+++ app/admin/location.rb
@@ -2,7 +2,6 @@ ActiveAdmin.register Location do
   index do
     selectable_column
     id_column
-    column :locationable_id
     column :locationable
     column :created_at
     column :latitude
