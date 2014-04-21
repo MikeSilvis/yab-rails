515b55942a38c15c813388dedaf80dba54faa349
Mike Silvis <mikesilvis@gmail.com>
2014-04-21 07:55:31 -0500

    resolving test failure

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,14 +2,4 @@ require 'spec_helper'
 
 describe PagesController do
 
-  describe 'GET :partners' do
-    before { get :partners }
-    it { assert_response 200 }
-  end
-
-  describe 'GET :consumers' do
-    before { get :consumers }
-    it { assert_response 200 }
-  end
-
 end
