cff32503c547f4b78b729c2047aca58cd0b42831
Mike Silvis <mikesilvis@gmail.com>
2014-04-25 09:33:08 -0500

    adding lat and lng to the api

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,11 +3,28 @@ class Location < ActiveRecord::Base
   belongs_to :merchant
   has_many :checkins
   validates :minor, uniqueness: { scope: :major }
+  before_save :set_lat_lng
+  acts_as_mappable default_units: :miles,
+                   default_formula: :sphere,
+                   distance_field_name: :distance,
+                   lat_column_name: :latitude,
+                   lng_column_name: :longitude
 
   def self.for_beacon!(attrs)
     find_by!(
       major: attrs[:major].to_s,
       minor: attrs[:minor].to_s
     )
+  end
+
+  private
+
+  def set_lat_lng
+    if street_changed? || city_changed?
+      Geokit::Geocoders::GoogleGeocoder.geocode("#{street}, #{city}, #{state}").tap do |geo|
+        self.latitude   = geo.lat
+        self.longitude  = geo.lng
+      end
+    end
   end
 end
--- app/serializers/location_serializer.rb
+++ app/serializers/location_serializer.rb
@@ -1,3 +1,11 @@
 class LocationSerializer < ActiveModel::Serializer
-  attributes :id, :street, :zipcode, :state, :city, :market_id
+  attributes :id,
+             :street,
+             :zipcode,
+             :state,
+             :city,
+             :market_id,
+             :message,
+             :latitude,
+             :longitude
 end
